f6be9fa16449dabff83d697d6a37ebf59bd9eff2
Jody Ririt Krido Suseno <jodysuseno@gmail.com>
2023-03-23 11:22:33 +0700

    add modal popup for certificate document and arrow top button

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,10 @@
 window.onscroll = function () {
   const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
-  
-  if(window.pageYOffset > fixedNav){
+
+  if (window.pageYOffset > fixedNav) {
     header.classList.add('navbar-fixed');
-  }else{
+  } else {
     header.classList.remove('navbar-fixed');
   }
 };
@@ -15,7 +15,13 @@ window.onscroll = function () {
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function(){
+hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
+// modal popup
+function toggleModal(id) {
+  document.getElementById('modal' + id).classList.toggle('hidden');
+  console.log('test');
+}
