661ddf703e397b0a7607058c9377ea02f0d333d4
jodysuseno <jodysuseno@gmail.com>
2023-10-04 18:09:35 +0700

    build: create categori page and rollback file index

--- dev/js/script.js
+++ dev/js/script.js
@@ -12,4 +12,15 @@ $(document).ready(function () {
     }, function () {
         document.getElementById('.carousel-hover').style.display = 'block';
     });
+
+    function isMobile() {
+        const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+        return regex.test(navigator.userAgent);
+    }
+    
+    if (isMobile()) {
+    console.log("Mobile device detected");
+    } else {
+    console.log("Desktop device detected");
+    }
 });
