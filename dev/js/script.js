e03ca34aac1761ee261455dfc06fb6f6e89c0bd9
jodysuseno <jodysuseno@gmail.com>
2023-10-12 17:54:12 +0700

    fix: fixing user interface based HPF Website Notes

--- dev/js/script.js
+++ dev/js/script.js
@@ -12,15 +12,54 @@ $(document).ready(function () {
     }, function () {
         document.getElementById('.carousel-hover').style.display = 'block';
     });
+    
+    // Tambahkan event listener untuk mengatur visibilitas tombol navigasi
+    $('#carouselItem').on('slid.bs.carousel', function () {
+        var $this = $(this);
 
-    function isMobile() {
-        const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-        return regex.test(navigator.userAgent);
+        // Cek apakah sedang pada slide pertama atau terakhir
+        if ($('.carousel-inner .carousel-item:first').hasClass('active')) {
+            $this.find('.carousel-control-prev').hide();
+            $this.find('.carousel-control-next').show();
+        } else if ($('.carousel-inner .carousel-item:last').hasClass('active')) {
+            $this.find('.carousel-control-prev').show();
+            $this.find('.carousel-control-next').hide();
+        } else {
+            $this.find('.carousel-control-prev').show();
+            $this.find('.carousel-control-next').show();
+        }
+    });
+
+    // Sembunyikan tombol prev secara default
+    $('.carousel-control-prev').hide();
+
+    // Menonaktifkan swipe ke kiri saat di slide pertama
+    $('#carouselItem').on('slide.bs.carousel', function (e) {
+        if (e.direction == 'right' && $('.carousel-inner .carousel-item:first').hasClass('active')) {
+            e.preventDefault();
+        }
+    });
+
+    // Menonaktifkan swipe ke kanan saat di slide terakhir
+    $('#carouselItem').on('slide.bs.carousel', function (e) {
+        if (e.direction == 'left' && $('.carousel-inner .carousel-item:last').hasClass('active')) {
+            e.preventDefault();
+        }
+    });
+    
+    window.addEventListener("resize", myFunction);
+    function myFunction() {
+        if (window.innerWidth < 992) {
+            $("#navLink1").removeAttr("href");
+            $("#navLink2").removeAttr("href");
+            $("#navLink3").removeAttr("href");
+            $("#navLink4").removeAttr("href");
+        } else {
+            $("#navLink1").attr("href","whiteboard.html");
+            $("#navLink2").attr("href","whiteboard.html");
+            $("#navLink3").attr("href","whiteboard.html");
+            $("#navLink4").attr("href","whiteboard.html");
+        }
     }
     
-    if (isMobile()) {
-    console.log("Mobile device detected");
-    } else {
-    console.log("Desktop device detected");
-    }
 });
